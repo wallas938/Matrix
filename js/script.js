c350db2378796c6f8b6b9d6153bd693a2b90c6a4
wallas938 <drame.sissako001@gmail.com>
2019-06-18 15:30:01 +0200

    matrix 1.0

--- js/script.js
+++ js/script.js
@@ -1,81 +1,137 @@
-$(function () {
+$(function() {
   /** Fonction pour remplir toute les formes de la page */
-  $('#btn-fill').click(function() {
-    $('.form').addClass('filled')
-  })
+  $("#btn-fill").click(function() {
+    resetAllState($(".form"));
+    $(".form").addClass("formsColor");
+  });
 
-    /** Fonction pour vider toute les formes de la page */
-    $('#btn-empty').click(function() {
-      $('.form').removeClass('filled')
-    })
+  /** Fonction pour vider toute les formes de la page */
+  $("#btn-empty").click(function() {
+    resetAllState($(".form"));
+    $(".form").addClass("noColorClass");
+  });
 
-    /** Fonction pour remplir un seul cercle */
-    $('.circle').click(function() {
-      $(this).toggleClass('filled')
-    })
+  /** Fonction pour remplir un seul cercle */
+  $(".circle").click(function() {
+    if ($(this).hasClass("circleColor")) {
+      resetAllState($(this));
+      $(this).removeClass("circleColor");
+    } else {
+      resetAllState($(this));
+      $(this).addClass("circleColor");
+    }
+  });
 
-    /** Fonction pour remplir un carre et sa rangé */
-    $('.squarre').click(function() {
-      let currentSuarre = $(this)
-      currentSuarre.toggleClass('filled')
-      let idParent = currentSuarre.parent().attr('id')
+  /** Fonction pour remplir un carre et sa rangé */
+  $(".squarre").click(function() {
+    let currentSuarre = $(this);
+    currentSuarre.toggleClass("squarreColor");
+    let idParent = currentSuarre.parent().attr("id");
 
-      switch (idParent) {
-        case 'row1':
-              if(currentSuarre.hasClass('filled'))
-                $('#row1').children().addClass('filled')
-              else 
-                $('#row1').children().removeClass('filled')
-          break;
-        case 'row2':
-            if(currentSuarre.hasClass('filled'))
-            $('#row2').children().addClass('filled')
-          else 
-            $('#row2').children().removeClass('filled')
-          break;
-        case 'row4':
-            if(currentSuarre.hasClass('filled'))
-            $('#row4').children().addClass('filled')
-          else 
-            $('#row4').children().removeClass('filled')
-          break;
-        case 'row5':
-            if(currentSuarre.hasClass('filled'))
-            $('#row5').children().addClass('filled')
-          else 
-            $('#row5').children().removeClass('filled')
-          break;
-        default:
-          break;
-      }
-    })
+    switch (idParent) {
+      case "row1":
+        if (currentSuarre.hasClass("squarreColor")) {
+          resetAllState($("#row1").children());
+          $("#row1")
+            .children()
+            .addClass("squarreColor");
+        } else {
+          resetAllState($("#row1").children());
+          $("#row1")
+            .children()
+            .removeClass("squarreColor");
+        }
+        break;
+      case "row2":
+        if (currentSuarre.hasClass("squarreColor")) {
+          resetAllState($("#row2").children());
+          $("#row2")
+            .children()
+            .addClass("squarreColor");
+        } else {
+          resetAllState($("#row2").children());
+          $("#row2")
+            .children()
+            .removeClass("squarreColor");
+        }
+        break;
+      case "row4":
+        if (currentSuarre.hasClass("squarreColor")) {
+          resetAllState($("#row4").children());
+          $("#row4")
+            .children()
+            .addClass("squarreColor");
+        } else {
+          resetAllState($("#row4").children());
+          $("#row4")
+            .children()
+            .removeClass("squarreColor");
+        }
+        break;
+      case "row5":
+        if (currentSuarre.hasClass("squarreColor")) {
+          resetAllState($("#row5").children());
+          $("#row5")
+            .children()
+            .addClass("squarreColor");
+        } else {
+          resetAllState($("#row5").children());
+          $("#row5")
+            .children()
+            .removeClass("squarreColor");
+        }
+        break;
+      default:
+        break;
+    }
+  });
 
-     /** Fonction pour remplir le losange, sa rangé et sa colonne */
-    $('.losange').click(function() {
+  /** Fonction pour remplir le losange, sa rangé et sa colonne */
+  $(".losange").click(function() {
+    $(".losange").toggleClass("losangeColor");
 
-      $('.losange').toggleClass('filled')
+    upToDateColKeeper(".col3");
 
-      upToDateColKeeper('.col3')
-
-      upToDateRowKeeper('#row3')
-
-    })
-})
+    upToDateRowKeeper("#row3");
+  });
+});
 
 function upToDateColKeeper(colName) {
-  for(let elem of $(colName)) {
-    if($('.losange').hasClass('filled'))
-    $(elem).addClass('filled')
-    else
-    $(elem).removeClass('filled')
+  for (let elem of $(colName)) {
+    if ($(".losange").hasClass("losangeColor")) {
+      resetAllState($(elem));
+
+      $(elem).addClass("losangeColor");
+    } else {
+      resetAllState($(elem));
+
+      $(elem).addClass("noColorClass");
+    }
   }
 }
 
 function upToDateRowKeeper(rowName) {
-  for(let elem of $(rowName).children()) {
-    if($('.losange').hasClass('filled'))
-    $(elem).addClass('filled')
-    else
-    $(elem).removeClass('filled')
+  for (let elem of $(rowName).children()) {
+    if ($(".losange").hasClass("losangeColor")) {
+      $(elem).removeClass("circleColor");
+      $(elem).removeClass("squarreColor");
+      $(elem).removeClass("formsColor");
+
+      $(elem).addClass("losangeColor");
+    } else {
+      $(elem).removeClass("losangeColor");
+      $(elem).removeClass("circleColor");
+      $(elem).removeClass("squarreColor");
+      $(elem).removeClass("formsColor");
+      $(elem).addClass("noColorClass");
+    }
   }
+}
+
+function resetAllState(elem) {
+  elem.removeClass("losangeColor");
+  elem.removeClass("circleColor");
+  elem.removeClass("squarreColor");
+  elem.removeClass("formsColor");
+  elem.removeClass("noColorClass");
 }
