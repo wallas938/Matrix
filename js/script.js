2625d61bd7fe7c558d6b12d772bf9b6ef1d67337
wallas938 <drame.sissako001@gmail.com>
2019-06-19 17:14:10 +0200

    new corrections

--- js/script.js
+++ js/script.js
@@ -1,80 +1,81 @@
-$(function() {
-
+$(function () {
   /** Informe l'utilisateur de la couleur qui sera utilisé */
-  $('.form').hover(function () {
-    if($(this).hasClass('circle')) 
-      $(this).toggleClass('circleAnimClass')
-    else if($(this).hasClass('squarre')) {
-      $(this).toggleClass('squarreAnimClass')
+  $(".form").hover(function () {
+    if ($(this).hasClass("circle")) $(this).toggleClass("circleAnimClass");
+    else if ($(this).hasClass("squarre")) {
+      $(this).toggleClass("squarreAnimClass");
       let currentSuarre = $(this);
       let idParent = currentSuarre.parent().attr("id");
       switch (idParent) {
         case "row1":
           if (currentSuarre.hasClass("squarre")) {
-              currentSuarre.toggleClass("squarreAnimClass");
-              $("#row1")
+            currentSuarre.toggleClass("squarreAnimClass");
+            $("#row1")
               .children()
               .toggleClass("squarreAnimClass");
           }
           break;
         case "row2":
           if (currentSuarre.hasClass("squarre")) {
-              currentSuarre.toggleClass("squarreAnimClass");
+            currentSuarre.toggleClass("squarreAnimClass");
             $("#row2")
               .children()
               .toggleClass("squarreAnimClass");
           }
           break;
         case "row4":
-            if (currentSuarre.hasClass("squarre")) {
-              currentSuarre.toggleClass("squarreAnimClass");
-              $("#row4")
+          if (currentSuarre.hasClass("squarre")) {
+            currentSuarre.toggleClass("squarreAnimClass");
+            $("#row4")
               .children()
               .toggleClass("squarreAnimClass");
           }
           break;
         case "row5":
-            if (currentSuarre.hasClass("squarre")) {
-              currentSuarre.toggleClass("squarreAnimClass");
-              $("#row5")
+          if (currentSuarre.hasClass("squarre")) {
+            currentSuarre.toggleClass("squarreAnimClass");
+            $("#row5")
               .children()
               .toggleClass("squarreAnimClass");
-            }
+          }
           break;
         default:
           break;
+      }
+    } else {
+      $(".losange").toggleClass("losageAnimClass");
+
+      upToDateColHover(".col3");
+
+      upToDateRowHover("#row3");
     }
-    
-  }else {
-    $('.losange').toggleClass("losageAnimClass")
-
-    upToDateColHover('.col3')
-
-    upToDateRowHover('#row3')
-  }
-});
-  $("#btn-fill").hover(function() {
-    $('.form').toggleClass('formsAnimClass')
+  });
+  
+  $("#btn-fill").hover(function () {
+    $(".form").toggleClass("formsAnimClass");
   });
   /********************* **************************/
 
-
   /** Fonction pour remplir toute les formes de la page */
-  $("#btn-fill").click(function() {
+  $("#btn-fill").click(function () {
     resetAllState($(".form"));
     $(".form").addClass("formsColor");
   });
   /********************* **************************/
 
-
   /** Fonction pour vider toute les formes de la page */
-  $("#btn-empty").click(function() {
+  $("#btn-empty").click(function () {
     resetAllState($(".form"));
     $(".form").addClass("noColorClass");
   });
   /********************* **************************/
+
+  $("#btn-empty").hover(function () {
+    $(".form").toggleClass("emptyAnimClass");
+  });
+  
   /** Fonction pour remplir un seul cercle */
-  $(".circle").click(function() {
+  $(".circle").click(function () {
     if ($(this).hasClass("circleColor")) {
       resetAllState($(this));
       $(this).removeClass("circleColor");
@@ -85,7 +86,7 @@ $(function() {
   });
   /********************* **************************/
   /** Fonction pour remplir un carre et sa rangé */
-  $(".squarre").click(function() {
+  $(".squarre").click(function () {
     let currentSuarre = $(this);
     currentSuarre.toggleClass("squarreColor");
     let idParent = currentSuarre.parent().attr("id");
@@ -106,12 +107,10 @@ $(function() {
         break;
       case "row2":
         if (currentSuarre.hasClass("squarreColor")) {
-          
           $("#row2")
             .children()
             .addClass("squarreColor");
         } else {
-          
           $("#row2")
             .children()
             .removeClass("squarreColor");
@@ -149,7 +148,7 @@ $(function() {
   });
   /********************* **************************/
   /** Fonction pour remplir le losange, sa rangé et sa colonne */
-  $(".losange").click(function() {
+  $(".losange").click(function () {
     $(".losange").toggleClass("losangeColor");
 
     upToDateColKeeper(".col3");
@@ -176,8 +175,7 @@ function upToDateColKeeper(colName) {
 
 function upToDateColHover(colName) {
   for (let elem of $(colName)) {
-    if(!$(elem).hasClass('losange'))
-      $(elem).toggleClass("losageAnimClass")
+    if (!$(elem).hasClass("losange")) $(elem).toggleClass("losageAnimClass");
   }
 }
 
@@ -201,8 +199,7 @@ function upToDateRowKeeper(rowName) {
 
 function upToDateRowHover(rowName) {
   for (let elem of $(rowName).children()) {
-    if(!$(elem).hasClass('losange'))
-    $(elem).toggleClass('losageAnimClass')
+    if (!$(elem).hasClass("losange")) $(elem).toggleClass("losageAnimClass");
   }
 }
 
